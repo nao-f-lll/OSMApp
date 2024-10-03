688cc39b0ea9a1a7e14649f7f983c63cbb332c74
Naoufal <mopsyro@duck.com>
2024-10-03 08:33:32 +0200

    fix merge conflict

--- app/src/main/java/com/project/osmapp/screens/ContactScreen.kt
+++ app/src/main/java/com/project/osmapp/screens/ContactScreen.kt
@@ -2,7 +2,9 @@ package com.project.osmapp.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,16 +14,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.project.osmapp.components.BottomNavigationBar
+import com.project.osmapp.components.TopBarComponent
+import com.project.osmapp.components.TrendImageListComponent
 
 
 @Composable
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 fun ContactScreen(navController: NavHostController) {
-    Scaffold(
-        bottomBar = { BottomNavigationBar(navController) }
+    Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text(text = "Pantalla de Contacto", modifier = Modifier.padding(16.dp))
+        TopBarComponent()
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+
         }
+        BottomNavigationBar(navController)
     }
 }
